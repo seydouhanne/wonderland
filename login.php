6c2fc03b808baf4966ff998a784dac2d653feb33
seydouhanne <75482672+seydouhanne@users.noreply.github.com>
2022-01-19 23:25:16 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se connecter</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<style></style>
+   
+</head>
+<body>
+<nav class="navbar navbar-light " style = "background-color: #141227;">
+  <a class="navbar-brand" href="#">
+    <img src="navlogo.png" width="180" height="30" class="d-inline-block align-top" alt="">
+    </a>
+    <nav class="nav nav-pills nav-fill w-25" id="nav"  >
+  <a class="nav-item nav-link active" style = "background-color: #B88A38;" href="#">PROFIL</a>
+  <a class="nav-item nav-link" style = "color: #B88A38;"  href="#">MARCHÉ</a>
+  <a class="nav-item nav-link" style = "color: #B88A38;"  href="#">MES PRODUITS</a>
+</nav>
+
+</nav>
+</body>
+</html>
